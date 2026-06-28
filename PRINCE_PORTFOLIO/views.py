7cf35c5c2bf3dd22cf7ea91ab6c83a950e2540d4
PrinceU <prince.ubando1228@gmail.com>
2026-06-29 07:47:40 +0800

    working html interconnectivityness

--- PRINCE_PORTFOLIO/views.py
+++ PRINCE_PORTFOLIO/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def works(request):
+    return render(request, 'works.html')
+
+def mworks(request):
+    return render(request, 'mworks.html')
+
+def contact(request):
+    return render(request, 'contact.html')
