1639a188ffd1d9e304ec4216ae382a4f1bd6ac79
PrinceU <prince.ubando1228@gmail.com>
2026-07-09 00:20:39 +0800

    Data of projects done, with url migrating to apps, personal data to follow

--- portDATA/views.py
+++ portDATA/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import ProjDATA, PersonDATA
 
-# Create your views here.
+def about(request):
+    personal_info = PersonDATA.objects.first()
+    return render(request, 'about.html', {'profile': personal_info})
+
+def project_list(request):
+    projects = ProjDATA.objects.all()
+    return render(request, 'works.html', {'ProjDATA': projects})
+
+def project_detail(request, project_id):
+    project = get_object_or_404(ProjDATA, pk=project_id)
+    return render(request, 'project_detail.html', {'project': project})
